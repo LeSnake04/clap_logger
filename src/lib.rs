e6329016b80316e61488faf0997080441f1a4ea1
LeSnake <dev.lesnake@posteo.de>
2022-08-30 19:10:20 +0200

    fixed examples

--- src/lib.rs
+++ src/lib.rs
@@ -29,21 +29,21 @@
 //!
 //! ## Adding the Argument
 //! ### Base Implementation:
-//! ```
+//! ```rust
 //! use clap::Command;
 //! use log::LevelFilter;
 //! use clap_logger::{ClapInitLogger, ClapLoglevelArg};
 //!
 //! // Generate a clap command
-//! let m: clap::ArgMatches = Command::new("clap_command_test")//!
+//! let m: clap::ArgMatches = Command::new("clap_command_test")
 //!   // add loglevel argument
-//!		.add_loglevel_arg()
+//!		.add_loglevel_arg(LevelFilter::Info)
 //! 	.get_matches();
 //! ```
 //!
 //! ## loglevel Arg manipulation
 //! You can also get the [Arg][clap::Arg] directly in order to modify it before adding:`
-//! ```
+//! ```rust
 //! use clap::{arg, Arg, Command};
 //! use log::LevelFilter;
 //! use clap_logger::{ClapInitLogger, get_loglevel_arg};
@@ -56,23 +56,23 @@
 //! 		.short('l')
 //!     // changing the long version of the argument just because I can
 //! 		.long("custom-loglevel")
-//!     // make it required to annoy the user
-//!     .required(true))
+//! 		.default_value("INFO")
+//!   )
 //! 	.get_matches();
 //! ```
 //! Warning: Do NOT touch `.possible_values`, `.id` field of the argument or anything in that modifies the input.
 //!
 //! ## Initialising the logger
 //! ### Base implementation:
-//! ```
+//! ```rust
 //! use clap::Command;
 //! use log::LevelFilter;
 //! use clap_logger::{ClapInitLogger, ClapLoglevelArg};
 //!
 //! let m: clap::ArgMatches = Command::new("clap_command_test")
 //!   // add the loglevel argument
-//!  	.add_loglevel_arg()
-//! 	.get_matches();//!
+//!  	.add_loglevel_arg(LevelFilter::Info)
+//! 	.get_matches();
 //!
 //! m.init_logger().expect("Failed to initialize logger");
 //! ```
